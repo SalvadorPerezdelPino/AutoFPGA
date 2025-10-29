0c94d3630dcb305fd2fd53afd087d1502a9b33e0
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2025-10-29 23:47:55 +0000

    Add Base Problem class

---
+++ source/base_problem.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+import json
+
+class BaseProblem(ABC):
+    """Interface for any problem"""
+
+    def __init__(self, config_file=None):
+        self.config = {}
+        if config_file:
+            self.load_config(config_file)
+
+    def load_config(self, json_file):
+        with open(json_file, "r") as file:
+            self.config = json.load(file)
+
+    
+    @abstractmethod
+    def generate_inputs(self):
+        pass
+
+    @abstractmethod
+    def solve(self):
+        pass
+
+    @abstractmethod
+    def save_result(self):
+        pass
