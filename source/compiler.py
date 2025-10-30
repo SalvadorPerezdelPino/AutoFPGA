6ea4b1bb7bcd98daaabbb4384bfc9c73e52b731a
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2025-10-30 15:24:13 +0000

    Add error detection in processes

--- source/compiler.py
+++ source/compiler.py
@@ -1,4 +1,3 @@
-import json
 import subprocess
 
 class QuartusCompiler:
@@ -9,42 +8,73 @@ class QuartusCompiler:
 
     def synthetize(self) -> None:
         if self.verbose:
-            subprocess.run(f"quartus_map {self.project_path}")
+            result = subprocess.run(f"quartus_map {self.project_path}")
         else:
-            subprocess.run(f"quartus_map {self.project_path}", 
+            result = subprocess.run(f"quartus_map {self.project_path}", 
                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if result.returncode != 0:
+            print("Synthesis failed.")
+            exit(result.returncode)
+        else:
+            print("Synthesis was successful!")
+
 
     def fit(self) -> None:
         if self.verbose:
-            subprocess.run(f"quartus_fit {self.project_path}")
+            result = subprocess.run(f"quartus_fit {self.project_path}")
         else:
-            subprocess.run(f"quartus_fit {self.project_path}", 
+            result = subprocess.run(f"quartus_fit {self.project_path}", 
                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if result.returncode != 0:
+            print("Fitting failed.")
+            exit(result.returncode)
+        else:
+            print("Fitting was successful!")
 
     def assemble(self) -> None:
         if self.verbose:
-            subprocess.run(f"quartus_asm {self.project_path}")
+            result = subprocess.run(f"quartus_asm {self.project_path}")
         else:
-            subprocess.run(f"quartus_asm {self.project_path}", 
+            result = subprocess.run(f"quartus_asm {self.project_path}", 
                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if result.returncode != 0:
+            print("Assembling failed.")
+            exit(result.returncode)
+        else:
+            print("Assembling was successful!")
 
     def timing(self) -> None:
         if self.verbose:
-            subprocess.run(f"quartus_sta {self.project_path}")
+            result = subprocess.run(f"quartus_sta {self.project_path}")
         else:
-            subprocess.run(f"quartus_sta {self.project_path}", 
+            result = subprocess.run(f"quartus_sta {self.project_path}", 
                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if result.returncode != 0:
+            print("Timing failed.")
+            exit(result.returncode)
+        else:
+            print("Timing was successful!")
 
     def netlist(self) -> None:
         if self.verbose:
-            subprocess.run(f"quartus_eda {self.project_path}")
+            result = subprocess.run(f"quartus_eda {self.project_path}")
         else:
-            subprocess.run(f"quartus_eda {self.project_path}", 
+            result = subprocess.run(f"quartus_eda {self.project_path}", 
                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if result.returncode != 0:
+            print("Netlist creation failed.")
+            exit(result.returncode)
+        else:
+            print("Netlist creation was successful!")
 
     def compile_all(self) -> None:
         if self.verbose:
-            subprocess.run(f"quartus_sh --flow compile {self.project_path}")
+            result = subprocess.run(f"quartus_sh --flow compile {self.project_path}")
         else:
-            subprocess.run(f"quartus_sh --flow compile {self.project_path}", 
+            result = subprocess.run(f"quartus_sh --flow compile {self.project_path}", 
                     stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if result.returncode != 0:
+            print("Compilation failed at some point.")
+            exit(result.returncode)
+        else:
+            print("Compilation was successful!")
