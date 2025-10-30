5e0b40b340d3231cec009c14bf2a8d4518c5d7e4
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2025-10-30 00:39:22 +0000

    Modify Knapsack to make it child of Base Problem

--- source/knapsack.py
+++ source/knapsack.py
@@ -1,81 +1,40 @@
+from base_problem import BaseProblem
 import random
-from datetime import datetime
+from pathlib import Path
 
-MEMORY_DEPTH = 2048
+class KnapsackProblem(BaseProblem):
+    def __init__(self, config_file=None) -> None:
+        super.__init__(config_file)
+        self.capacity = config_file.get("capacity", 50)
+        self.items = config_file.get("items", 20)
+        self.values = []
+        self.weights = []
+        self.result = None
 
-class Knapsack:
-    def __init__(self) -> None:
-        self.capacity = None
-        self.items = None
-        self.weights = None
-        self.values = None
-        self.solution = None
+    def generate_inputs(self):
+        self.values = [random.randint(1, 100) for _ in range(self.items)]
+        self.weights = [random.randint(self.capacity/4, self.capacity/2) for _ in range(self.items)]
+        print(f"Values: {self.values}")
+        print(f"Weights: {self.weights}")
 
-    def set_seed(self, new_seed: int) -> None:
-        self.seed = new_seed
-        random.seed(self.seed)
+    def save_inputs(self):
+        pass
 
-    def generate_random_seed(self) -> None:
-        self.seed = int(datetime.microsecond)
-        random.seed(self.seed)
+    def solve(self):
+        dp = [[0] * (self.capacity + 1) for _ in range(self.items + 1)]
 
-    def generate_weights(self, capacity: int, items: int) -> list:
-        weights = [random.randint(int(capacity/4), int(capacity/2)) for _ in range(items)]
-        return weights
-
-    def generate_values(self, items: int) -> list:
-        values = [random.randint(1, 100) for _ in range(items)]
-        return values
-
-    def solve_knapsack(self, weights: list, values: list, capacity: int, items: int):
-        n = items
-        dp = [[0] * (capacity + 1) for _ in range(n + 1)]
-
-        for i in range(1, n + 1):
-            for w in range(capacity + 1):
-                if weights[i-1] <= w:
-                    dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]] + values[i-1])
+        for i in range(1, self.items + 1):
+            for w in range(self.capacity + 1):
+                if self.weights[i - 1] <= w:
+                    dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - self.weights[i - 1]] + self.values[i - 1])
                 else:
-                    dp[i][w] = dp[i-1][w]
+                    dp[i][w] = dp[i - 1][w]
 
-        w = capacity
-        selection = [0] * n
-        for i in range(n, 0, -1):
-            if dp[i][w] != dp[i-1][w]:
-                selection[i-1] = 1
-                w -= weights[i-1]
+        self.result = dp[self.items][self.capacity]
+        return self.result
 
-        return dp[n][capacity], selection
-
-    def generate_problem(self, capacity: int, items: int):
-        self.items = items
-        self.capacity = capacity
-        self.weights = self.generate_weights(capacity=self.capacity, 
-                items=self.items)
-        self.values = self.generate_values(items=self.items)
-        self.solution, _ = self.solve_knapsack(weights=self.weights, 
-                values=self.values, capacity=self.capacity, items=self.items)
-    
-    def __int_to_bin_str(self, num: int, bits=16) -> str:
-        return format(num, f'0{bits}b')
-    
-    def write_problem_file(self, filename: str):
-        if None in (self.capacity, self.items, self.weights, self.values):
-            raise ValueError("Problema no generado. Utilizar generate_problem() primero.")
-
-        size = MEMORY_DEPTH - 2 - 2* len(self.weights)
-        with open(filename, "w+") as f:
-            f.write(self.__int_to_bin_str(self.items) + "\n")
-            f.write(self.__int_to_bin_str(self.capacity) + "\n")
-            for w in self.weights:
-                f.write(self.__int_to_bin_str(w) + "\n")
-            for v in self.values:
-                f.write(self.__int_to_bin_str(v) + "\n")
-            for _ in range(size):
-                f.write(self.__int_to_bin_str(0) + "\n")
-
-    def write_solution_file(self, filename: str):
-        if self.solution is None:
-            raise ValueError("Solución no generada. Utilizar generate_problem() primero.")
-        with open(filename, "w+") as f:
-            f.write(self.__int_to_bin_str(self.solution) + "\n")
+    def save_result(self, save_path):
+        Path(save_path).parent.mkdir(parents=True, exist_ok=True)
+        with open(save_path, "w") as f:
+            f.write(f"{self.result:016b}\n")
+        print(f"Result stored in {save_path}")
