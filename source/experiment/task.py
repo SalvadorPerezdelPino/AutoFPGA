acbc2dfab41a932bc107e5468786eb82828e75bb
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2026-02-01 11:33:28 +0000

    Allow parallel sweep

--- source/experiment/task.py
+++ source/experiment/task.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Union
 from pathlib import Path
 import itertools
 
@@ -28,54 +28,70 @@ class TaskBuilder():
         
         return tasks
     
+    def _resolve_values(self, val: Union[Dict, List, Any]) -> List[Any]:
+        if isinstance(val, dict) and "start" in val:
+            step = val.get("step", 1)
+            return list(range(val["start"], val["stop"] + 1, step))
+        elif isinstance(val, list):
+            return val
+        else:
+            return [val]
+    
     def _generate_tasks_for_experiment(self, exp_cfg: dict, problems_config: dict) -> List[Task]:
         generated = []
         exp_name = exp_cfg.get("name", "unnamed")
         problem_name = exp_cfg.get("problem")
         devices = exp_cfg.get("devices", [])
-        sweep_vars = exp_cfg.get("sweep_vars", {})
 
         base_params = problems_config.get(problem_name, {}).copy()
 
-        param_keys = []
-        param_values_list = []
+        sweep_vars = exp_cfg.get("sweep_vars", {})
+        sweep_keys = list(sweep_vars.keys())
+        sweep_values = [self._resolve_values(val) for val in sweep_vars.values()]
+        
+        sweep_combinations = []
+        if sweep_vars:
+            for combo in itertools.product(*sweep_values):
+                sweep_combinations.append(dict(zip(sweep_keys, combo)))
+        else:
+            sweep_combinations = [{}]
 
-        for key, val in sweep_vars.items():
-            param_keys.append(key)
+        coupled_cfg = exp_cfg.get("coupled_vars", {})
+        coupled_combinations = []
+        
+        if coupled_cfg and "keys" in coupled_cfg and "range" in coupled_cfg:
+            target_keys = coupled_cfg["keys"]
+            range_cfg = coupled_cfg["range"]
             
-            if isinstance(val, dict) and "start" in val:
-                step = val.get("step", 1)
-                r = range(val["start"], val["stop"] + 1, step) 
-                param_values_list.append(list(r))
-            elif isinstance(val, list):
-                param_values_list.append(val)
-            else:
-                param_values_list.append([val])
-
-        combinations = list(itertools.product(*param_values_list))
+            values_list = self._resolve_values(range_cfg) 
+            
+            for val in values_list:
+                param_dict = {k: val for k in target_keys}
+                coupled_combinations.append(param_dict)
+                
+        else:
+            coupled_combinations = [{}]
 
         run_counter = 0
 
-        for values in combinations:
-            current_sweep_params = dict(zip(param_keys, values))
-            
-            final_params = {**base_params, **current_sweep_params}
-            
-            for device in devices:
-                run_id = f"{run_counter:04d}"
-                # TODO: Choose correct task path
-                task_dir = self.context / exp_name / device / run_id
+        for s_params in sweep_combinations:
+            for c_params in coupled_combinations:
+                final_params = {**base_params, **s_params, **c_params}
                 
-                new_task = Task(
-                    id=run_id,
-                    name=exp_name,
-                    device_name=device,
-                    problem_name=problem_name,
-                    params=final_params,
-                    output_dir=task_dir
-                )
-                generated.append(new_task)
-            
-            run_counter += 1
+                for device in devices:
+                    run_id = f"{run_counter:04d}"
+                    task_dir = self.context / exp_name / device / run_id
+                    
+                    new_task = Task(
+                        id=run_id,
+                        name=exp_name,
+                        device_name=device,
+                        problem_name=problem_name,
+                        params=final_params,
+                        output_dir=task_dir
+                    )
+                    generated.append(new_task)
+                
+                run_counter += 1
 
         return generated
