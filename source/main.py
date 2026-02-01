5e684088295e2463ced84f8b16c469dcd0a266d9
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2026-02-01 12:10:47 +0000

    Add arguments to provide configuration path, verbose, experiment running and logging path

--- source/main.py
+++ source/main.py
@@ -1,15 +1,31 @@
 from core.config import load_config
 import logging
-
+import argparse
 from experiment.runner import ExperimentRunner
+import os
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+parser = argparse.ArgumentParser(prog="AutoFPGA", description="Automatic experiments for FPGA")
+parser.add_argument('-c', '--config', default="./config/config.json")
+parser.add_argument('-r', '--recover') # TODO: TBD
+parser.add_argument('-v', '--verbose', action='store_true') 
+parser.add_argument('-e', '--experiment', action='store_true')
+parser.add_argument('-l', '--logging', default="./logging/latest.log")
+args = parser.parse_args()
 
-config = load_config("./config/config.json")
+logger = logging.getLogger('AutoFPGA')
+os.makedirs(os.path.dirname(args.logging), exist_ok=True)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=args.logging,
+    filemode='w',
+    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
-verbose = True
+logger.info("Program started!")
+logger.debug(f"Config file is {args.config}")
+config = load_config(args.config)
 
-runner = ExperimentRunner(config=config, verbose=verbose)
-
-runner.run()
+if args.experiment:
+    runner = ExperimentRunner(config=config, verbose=args.verbose)
+    runner.run()
