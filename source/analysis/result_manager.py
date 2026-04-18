595d73c313970efb261310e7ef92dad38b8b96d7
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2026-04-18 15:44:38 +0100

    Add recovery improvements

--- source/analysis/result_manager.py
+++ source/analysis/result_manager.py
@@ -2,6 +2,7 @@ import logging
 import pandas as pd
 from pathlib import Path
 from typing import List
+import os
 
 logger = logging.getLogger('Result Manager')
 
@@ -28,6 +29,21 @@ class ResultManager:
         path.parent.mkdir(parents=True, exist_ok=True)
         self._master_df.to_csv(path, index=False, sep=';', decimal=',')
         logger.info(f"Results saved to {path}")
+
+    def recover_csv(self, search_path: Path, output_path: Path):
+        csv_paths = self.get_all_csv(search_path)
+        for csv in csv_paths:
+            df = pd.read_csv(csv, decimal=',', sep=';')
+            self.add(df)
+        self.save(output_path)
+
+    def get_all_csv(self, path: Path):
+        csv_paths = []
+        for p, d, f in os.walk(path):
+            for file in f:
+                if file.endswith('.csv'):
+                    csv_paths.append(Path(p) / file)
+        return csv_paths
 
     @property
     def df(self):
